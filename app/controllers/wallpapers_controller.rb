ac821024b84c530fa827b6d51752c1e86f8f5849
Arjun <akasana8@gmail.com>
2017-07-16 13:56:12 +0530

    16 th july

--- app/controllers/wallpapers_controller.rb
+++ app/controllers/wallpapers_controller.rb
@@ -1,10 +1,41 @@
  class WallpapersController<ApplicationController
-def home
-end
+
  	def index
       @wallpapers=Wallpaper.all
  	end
 def show
 	@wallpaper=Wallpaper.find(params[:id])
 end
- end
+def new
+	@wallpaper=Wallpaper.new
+end
+def create
+@wallpaper=Wallpaper.new(wallpaper_params)
+@wallpaper.user=User.find(1)
+if @wallpaper.save
+	flash[:success]="Your wallpaper was added succesfully"
+	redirect_to wallpapers_path
+	#do something
+else
+	render :new
+end
+end 
+def edit
+	@wallpaper=Wallpaper.find(params[:id])
+	
+end
+def update
+	@wallpaper=Wallpaper.find(params[:id])
+	if @wallpaper.update(wallpaper_params)
+		#do something
+	flash[:success]="Your wallpaper was edited and updated!"
+	redirect_to wallpaper_path(@wallpaper)
+	else
+		render :edit
+	end
+end
+private 
+  def wallpaper_params
+   params.require(:wallpaper).permit(:name,:summary,:description) 
+  end
+	end
